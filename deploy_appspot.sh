9400e8f6b0e7e0a096aee1d86b65253c8acc1307
Mark Sherman <msherman@cs.uml.edu>
2016-05-13 12:08:43 -0400

    added oath2 for appspot deployment

--- deploy_appspot.sh
+++ deploy_appspot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-appid=mark-ai
+#appid=mark-ai
+appid=ai2snapshot
 appinventor=/Users/mark/android/appinventor-sources/appinventor
 SDK=/Users/mark/android/appengine-java-sdk-1.9.27
 
-$SDK/bin/appcfg.sh -A $appid update $appinventor/appengine/build/war/
+$SDK/bin/appcfg.sh -A $appid --oauth2 update $appinventor/appengine/build/war/
 

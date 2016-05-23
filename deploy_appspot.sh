2b7a60821a10471313c3a4bf8ba8256185103f78
Mark Sherman <msherman@cs.uml.edu>
2016-05-23 12:04:42 -0400

    update to appengine-java-sdk-1.9.37

--- deploy_appspot.sh
+++ deploy_appspot.sh
@@ -2,7 +2,7 @@
 #appid=mark-ai
 appid=ai2snapshot
 appinventor=/Users/mark/android/appinventor-sources/appinventor
-SDK=/Users/mark/android/appengine-java-sdk-1.9.27
+SDK=/Users/mark/android/appengine-java-sdk-1.9.37
 
 $SDK/bin/appcfg.sh -A $appid --oauth2 update $appinventor/appengine/build/war/
 
--- start_server.sh
+++ start_server.sh
@@ -4,7 +4,7 @@
 # Uses the App Engine dev server in the SDK
 
 appinventor=/Users/mark/android/appinventor-sources/appinventor
-SDK=/Users/mark/android/appengine-java-sdk-1.9.27
+SDK=/Users/mark/android/appengine-java-sdk-1.9.37
 
 echo "Starting local App Inventor dev server"
 

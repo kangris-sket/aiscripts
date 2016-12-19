bb8a2768dabb452415417121043514d15715314d
Mark Sherman <msherman@cs.uml.edu>
2016-12-19 13:35:40 -0500

    Appspot deploy asks for version code

--- deploy_appspot.sh
+++ deploy_appspot.sh
@@ -7,5 +7,19 @@ BASEDIR=/Users/mark/android
 appinventor=$BASEDIR/appinventor-sources/appinventor
 SDK=$BASEDIR/appengine-java-sdk-1.9.37
 
-$SDK/bin/appcfg.sh -A $appid --oauth2 update $appinventor/appengine/build/war/
+echo "##################################################"
+branch=`git -C $appinventor rev-parse --abbrev-ref HEAD`
+echo "Current branch is: $branch" 
+echo "Current deploy instance is: $appid"
+
+echo "Do you wish to add a version code?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) vim '+normal 4G12|' $appinventor/appengine/build/war/WEB-INF/appengine-web.xml; break;;
+        No ) break;;
+    esac
+done
+
+echo "Deploying..."
+$SDK/bin/appcfg.sh -A $appid update $appinventor/appengine/build/war/
 
